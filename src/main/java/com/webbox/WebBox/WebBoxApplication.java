ca062a22aaa4a68f9484b876014d54cbc53a96b8
周腾 <251027705@qq.com>
2023-02-11 10:57:37 +0800

    init commit

---
+++ src/main/java/com/webbox/WebBox/WebBoxApplication.java
@@ -0,0 +1,13 @@
+package com.webbox.WebBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webbox/WebBox/WebBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbox.WebBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
